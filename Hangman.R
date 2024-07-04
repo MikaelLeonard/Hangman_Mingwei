f514b3989897e2a8b5984e31b6acf3c69fdaaaad
MikaelLeonard <mikael.gouwtama@mail.utoronto.ca>
2024-07-03 20:23:40 -0400

    Added more comments on the game setup

--- Hangman.R
+++ Hangman.R
@@ -5,15 +5,19 @@
 ##     guess before congratulating it.
 
 ## MG: I also notice that your input confuses some special characters like spaces,
-##    \, and | doesn't work.
+##     \, and | doesn't work.
 
 allword<- unlist(read.csv("Words.txt"))
 #Reading the dictionary file that is stored in the same folder/repo
 #sample() cannot process list as an input we therefore had to unlist the dictionary
 
+## MG: Great use of the sample() function to choose a word
 word <- sample(allword,1)
 #choosing 1 word from the dictionary that we just made into a vector 
 
+## MG: This is really smart. I think splitting the word into a string of characters 
+##     allow for an easier manipulation such as for comparison and chekcing if a 
+##     letter is in the word.
 split_w <- unlist(strsplit(word, ""))
 #The word is then separated into a string of characters with strsplit() 
 #strsplit() gives a list as an output but I don't like that, it makes subsequent design difficult
@@ -22,17 +26,19 @@ split_w <- unlist(strsplit(word, ""))
 length_w <- length(split_w)
 #The length of split_w that is required for the subsequent replace()
 
+## MG: Nice work here, its nice being able to see the progress so far
 split_g<- replace(split_w, 1:length_w, "_")
 #g stands for game, in oppose to w which stands for words. 
 #I am replacing each character in split_g with a _ to be displayed during the game. 
 
-
+## MG: Great introduction to the game and the rules!
 cat("Welcome to Hangman! 
 For each round you get to make seven unsuccessful attempt
 If you are unable to guess the word before using up all seven attemtps the men would be hanged :( 
 To exit the game type Exit when being promted your guess.
 Your word has",length_w, "characters", split_g, 
 "Have fun!")
+
 
 i <- 1
 #i is set to 1 to in the beginning of the while loop
